ae3e18c10add6bbcaf5192a40624b1b332c2de98
Dylan Ferragame <50226788+Dylster99@users.noreply.github.com>
2019-10-24 03:56:52 -0700

    Add files via upload

---
+++ Friendfinder/app/data/friends.js
@@ -0,0 +1,102 @@
+const friends = [
+  {
+    name: "Asmongold",
+    photo: "https://i.ibb.co/0hst7d4/download-5.jpg",
+    scores: [
+      "5",
+      "3",
+      "5",
+      "1",
+      "1",
+      "3",
+      "4",
+      "2",
+      "4",
+      "5"
+    ]
+  },
+  {
+    name: "Mcconnel",
+    photo: "https://i.ibb.co/XCH8sN7/download-4.jpg",
+    scores: [
+      "4",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5",
+      "1",
+      "1",
+      "2",
+      "4"
+    ]
+  },
+  {
+    name: "Pshero",
+    photo: "https://i.ibb.co/gdk3Hx0/download-3.jpg",
+    scores: [
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Savix",
+    photo: "https://i.ibb.co/JsnnM8x/download-2.jpg",
+    scores: [
+      "2",
+      "3",
+      "5",
+      "1",
+      "5",
+      "2",
+      "3",
+      "2",
+      "4",
+      "3"
+    ]
+  },
+  {
+    name: "Filthy Frank",
+    photo: "https://i.ibb.co/qNk405W/images-1.jpg",
+    scores: [
+      "4",
+      "4",
+      "4",
+      "4",
+      "1",
+      "5",
+      "2",
+      "2",
+      "3",
+      "5"
+    ]
+  },
+  {
+    name: "idubbz",
+    photo: "https://i.ibb.co/vsv5MpB/download.jpg",
+    scores: [
+      "5",
+      "2",
+      "1",
+      "3",
+      "5",
+      "4",
+      "3",
+      "1",
+      "1",
+      "2"
+    ]
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
